a799e26e4fc64648cf6d68ef15c9462c8415da10
Tianhao Zhou <tianhaoz@umich.edu>
2020-12-09 07:53:30 +0000

    chore: add method for retrieving config content

--- src/utils/cred/from_yaml_config_impl.py
+++ src/utils/cred/from_yaml_config_impl.py
@@ -1,6 +1,11 @@
 from src.utils.cred.from_env_impl import AlpacaCredFromEnv
+from abc import abstractmethod
 
 
 class AlpacaCredFromYamlConfig(AlpacaCredFromEnv):
+    @abstractmethod
+    def read_config_content(self):
+        pass
+
     def get_cred(self, cred_type):
         raise NotImplementedError()
